934d85ee4e8066a0c53b7862603d7c9a3633e26c
Luis Martin MV <l20121013@morelia.tecnm.mx>
2023-05-14 22:48:30 -0600

    Agregada la funcion del menu desplegable

--- JS/funcion-menu.js
+++ JS/funcion-menu.js
@@ -1,16 +1,52 @@
-let listElemnts = document.querySelectorAll('.list_button--click')
+let listElemnts = document.querySelectorAll('.list_button--click-1')
 
 listElemnts.forEach(listElement => {
     listElement.addEventListener('click',()=>{
         
-        listElement.classList.toggle('arrow');
+        var a = listElement.classList.toggle('arrow');
 
-        let height =  0;
-        let menu = listElement.nextElementSibling;
-        if(menu.clientHeight == "0" ){
-            height=menu.scrollHeight;
+        if(a == true){
+            let mostrar = document.getElementById("m1");
+            mostrar.style.display = "block";
+        }else{
+            var mostrar = document.getElementById("m1");
+            mostrar.style.display = "none";
         }
-        console.log(menu.scrollHeight);
-        menu.style.height = `${height}px`;
     })
 })
+
+let listElemnts1 = document.querySelectorAll('.list_button--click-2')
+
+listElemnts1.forEach(listElement => {
+    listElement.addEventListener('click',()=>{
+        
+        var a = listElement.classList.toggle('arrow');
+
+        if(a == true){
+            let mostrar = document.getElementById("m2");
+            mostrar.style.display = "block";
+        }else{
+            var mostrar = document.getElementById("m2");
+            mostrar.style.display = "none";
+        }
+    })
+})
+
+
+let listElemnts3 = document.querySelectorAll('.list_button--click-3')
+
+listElemnts3.forEach(listElement => {
+    listElement.addEventListener('click',()=>{
+        
+        var a = listElement.classList.toggle('arrow');
+
+        if(a == true){
+            let mostrar = document.getElementById("m3");
+            mostrar.style.display = "block";
+        }else{
+            var mostrar = document.getElementById("m3");
+            mostrar.style.display = "none";
+        }
+    })
+})
+
