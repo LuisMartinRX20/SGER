e22b3953dd1e04b634859fb3193d9a37c7c3a799
Carlos <l20121019@morelia.tecnm.mx>
2023-05-21 15:44:13 -0600

    se agrego diseñp a info ficha

--- PHP/infoficha.php
+++ PHP/infoficha.php
@@ -5,18 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SGER: INFO FICHA</title>
+    <link href="../CSS/infoficha.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="contenedor">
         <div class="info">
-            <tr>
-                <li>No Ficha</li>
-                <li>Nombre Aspirante</li>
-                <li>CURP</li>
-                <li>Nombre Padre/Tutor</li>
-                <li>Fecha de Pago</li>
-                <li>Precio</li>
-            </tr>
+            <ul>
+                <li>No Ficha:</li>
+                <li>Nombre Aspirante:</li>
+                <li>CURP:</li>
+                <li>Nombre Padre/Tutor:</li>
+                <li>Fecha de Pago:</li>
+                <li>Precio:</li>
+            </ul>
         </div>
     </div>
 </body>
