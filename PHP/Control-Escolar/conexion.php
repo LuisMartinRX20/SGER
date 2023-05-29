604d1dfe63ec18e38a02d0018e9f219325b6ad87
Carlos <l20121019@morelia.tecnm.mx>
2023-05-29 04:34:53 -0600

    menu aviso jala

--- PHP/Control-Escolar/conexion.php
+++ PHP/Control-Escolar/conexion.php
@@ -3,7 +3,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "sger";
+$db_name = "sgerv1";
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if(mysqli_connect_errno()){
